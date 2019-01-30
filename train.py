1049d977cd46239ef585ff320ad68676acb37bb7
smi <s0558366@htw-berlin.de>
2019-01-30 23:50:42 +0100

    trainig script should work

--- train.py
+++ train.py
@@ -3,6 +3,14 @@ import os
 import sys
 import argparse
 
+import pandas as pd
+import numpy as np
+from gensim.models import Word2Vec
+
+from model.preprocessor import *
+
+pretrained_dir = 'pretrained-models/'
+data_dir = 'data/'
 
 def main(arguments):
     parser = argparse.ArgumentParser(
@@ -11,24 +19,30 @@ def main(arguments):
     parser.add_argument('-o', '--outfile', help='output file in which the Word2Vec Model will be saved (every model will be saved at the pretrained-models directory) - default name is word2vec.model',
                         default='word2vec.model')
     parser.add_argument('-d', '--data', help='dataset on which the word2vec model will be trained on (must be a CSV with the label text and in the data directory)  - default is the police report datasetl',
-                        default='data/police_reports.csv')
-    parser.add_argument('-w', '--workers', help='how many workes will be used ',
+                        default='police_reports.csv')
+    parser.add_argument('-wk', '--worker', help='how many workes will be used ',
                         default=1)
+    parser.add_argument('-wi', '--window', help='maximum distance between the current and predicted word within a sentence', required=True)
     parser.add_argument('-s', '--size', help='dimensionality of the word vectors.',
                         default=100)
     parser.add_argument('-m', '--mincount', help='how many words will be ignored with the total frequency lower than this number',
                         default=1)
     parser.add_argument('-e', '--epoch', help='number of iterations over the corpus - how often our model will see our dataset',
                         default=100)
-
     args = parser.parse_args(arguments)
     print(args)
-    #args.epoch
-    #args.mincount
-    #args.outfile
-    #args.size
-    #args.workers
-
+    # data preparation
+    df = pd.read_csv(data_dir + args.data)
+    raw_texts = df.text.get_values()
+    cleaned_texts = clean_document(raw_texts)
+    tokenized = tokenize_documents(cleaned_texts)
+    words = get_vocabulary(tokenized)
+    # actuall training phase
+    print('started training')
+    model = Word2Vec(tokenized, size=args.size, window=args.window, min_count=args.mincount, workers=args.worker)
+    model.train(tokenized, total_examples=model.corpus_count, epochs=args.epoch)
+    print('training ended - model will be safed at', pretrained_dir + args.outfile)
+    model.save(pretrained_dir + args.outfile)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
