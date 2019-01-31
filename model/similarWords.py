65261154025a25378a7762ff7551d552577467bf
smi <s0558366@htw-berlin.de>
2019-01-31 17:26:45 +0100

    bugfixing

---
+++ model/similarWords.py
@@ -0,0 +1,16 @@
+from gensim.models import Word2Vec
+import os
+class SimilarWords:
+    
+    def __init__(self, config):
+        self.model_name = config.pop('model_name')
+        self.cwd = os.getcwd()
+        self.model = Word2Vec.load(self.cwd + '/pretrained-models/' + self.model_name)
+        
+    def get_positive_similar_words(self, word, topn = 5):
+        if self.model.wv.vocab.get(word, 0) is not 0:
+            return self.model.wv.most_similar_cosmul(positive=word, topn=topn)
+    
+    def get_negative_similar_words(self, word, topn = 5):
+        if self.model.wv.vocab.get(word, 0) is not 0:
+            return self.model.wv.most_similar_cosmul(positive=word, negative=word, topn=topn)
