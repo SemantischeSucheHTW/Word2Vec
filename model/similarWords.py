8697a0466346d20ec8619d6458a7a6a0b5949ec0
smi <s0558366@htw-berlin.de>
2019-02-03 00:10:05 +0100

    added a drop rate

--- model/similarWords.py
+++ model/similarWords.py
@@ -9,9 +9,16 @@ class SimilarWords:
         self.model = Word2Vec.load(self.cwd + '/pretrained-models/' + self.model_name)
     
     
-    def get_similar_words(self, words, topn = 5):
-        result = []
+    def get_similar_words(self, words, topn = 5, drop = 0.4):
+        result_with_drop = []
+        result_without_drop = []
         for word in words:
             if self.model.wv.vocab.get(word, 0) is not 0:
-                result.append(self.model.wv.most_similar(positive=[word], topn=topn))
-        return result 
+                # get all words with the 5 highest cosine similarity
+                temp = self.model.wv.most_similar(positive=[word], topn=topn)
+                result_without_drop.append(temp)
+                # remove every word with a lower cosine similarity than drop
+                for word, similarity in temp:
+                    if similarity >= 0.4:
+                        result_with_drop.append((word, similarity))
+        return result_with_drop, result_without_drop 
